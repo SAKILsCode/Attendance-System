5c005dc2e8fda1e7babd43bfd9b8b9935c6e4003
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2022-07-22 02:33:20 +0600

    user CRUD operation added

--- server/service/auth.js
+++ server/service/auth.js
@@ -4,14 +4,20 @@ const jwt = require('jsonwebtoken');
 const error = require('../utils/error');
 const { findUserByProperty, createNewUser } = require('../service/user');
 
-const registerService = async ({ name, email, password }) => {
+const registerService = async ({
+  name,
+  email,
+  password,
+  roles,
+  accountStatus,
+}) => {
   const user = await findUserByProperty('email', email);
   if (user) throw error('User already exist', 400);
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  return createNewUser({ name, email, password: hash });
+  return createNewUser({ name, email, password: hash, roles, accountStatus });
 };
 
 const loginService = async ({ email, password }) => {
